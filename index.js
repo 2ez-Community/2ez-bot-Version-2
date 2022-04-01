ac12f4659e19dc5c2947d61aed80f7037b6e6e86
xTheFreeZe <66825996+xTheFreeZe@users.noreply.github.com>
2022-04-01 14:40:44 +0200

    :coffee: test

--- index.js
+++ index.js
@@ -53,7 +53,7 @@ client.on('interactionCreate', async interaction => {
 	} catch (error) {
 		console.error(error);
 		await interaction.reply({
-			content: 'There was an error while executing this command!',
+			content: 'There was an error while executing this command.',
 			ephemeral: true
 		});
 	}
