9c0ab82aefa934ac74fce04cb9739145e537edd1
xTheFreeZe <66825996+xTheFreeZe@users.noreply.github.com>
2022-06-04 21:34:29 +0200

    👋🌌Finished up `Poll.js`

--- commands/Poll.js
+++ commands/Poll.js
@@ -37,6 +37,7 @@ module.exports = {
 
 		const PollEmbed = new MessageEmbed()
 			.setTitle(`${PollQuestion}`)
+			.setColor('RANDOM')
 			.setDescription(`A poll by **${interaction.member.user.username}**`)
 			.addFields({
 				name: `${ChoiceOne}`,
@@ -66,6 +67,12 @@ module.exports = {
 				.setCustomId('EnableMultiChoice')
 				.setLabel('Enable Multi-Choice')
 				.setStyle('SUCCESS'),
+			)
+			.addComponents(
+				new MessageButton()
+				.setCustomId('StopPoll')
+				.setLabel('Close the Poll')
+				.setStyle('DANGER'),
 			);
 
 		interaction.reply({
@@ -88,6 +95,10 @@ module.exports = {
 				});
 
 				const EnableMultiChoiceCollector = sentMessage.createMessageComponentCollector({
+					componentType: 'BUTTON'
+				});
+
+				const StopPollCollector = sentMessage.createMessageComponentCollector({
 					componentType: 'BUTTON'
 				});
 
@@ -143,6 +154,7 @@ module.exports = {
 
 						const NewPollEmbed = new MessageEmbed()
 							.setTitle(`${PollQuestion}`)
+							.setColor('RANDOM')
 							.setDescription(`A poll by **${interaction.member.user.username}**`)
 							.addFields({
 								name: `${ChoiceOne}`,
@@ -218,6 +230,7 @@ module.exports = {
 
 						const NewPollEmbed = new MessageEmbed()
 							.setTitle(`${PollQuestion}`)
+							.setColor('RANDOM')
 							.setDescription(`A poll by **${interaction.member.user.username}**`)
 							.addFields({
 								name: `${ChoiceOne}`,
@@ -246,6 +259,14 @@ module.exports = {
 
 					if (i.customId === "EnableMultiChoice") {
 
+						if (i.user.id !== interaction.member.user.id) {
+							i.reply({
+								content: "You are not able to use this!",
+								ephemeral: true
+							});
+							return;
+						};
+
 						MultiChoiceConfig = "true";
 
 						i.reply({
@@ -254,6 +275,87 @@ module.exports = {
 						});
 
 					};
+				});
+
+				StopPollCollector.on('collect', i => {
+
+					if (i.customId === "StopPoll") {
+
+						if (i.user.id !== interaction.member.user.id) {
+							i.reply({
+								content: "You are not able to use this!",
+								ephemeral: true
+							});
+							return;
+						};
+
+						let CutChoiceOneVoters = ChoiceOneVoters.toString().replace(/,/g, "").replace(/\n/g, '').replace(/No votes yet/, '').trim().split(" ");
+						let CutChoiceTwoVoters = ChoiceTwoVoters.toString().replace(/,/g, "").replace(/\n/g, '').replace(/No votes yet/, '').trim().split(" ");
+
+						if (ChoiceOneVoters === 'No votes yet') {
+							ChoiceOneVoters = 0;
+						} else {
+							ChoiceOneVotersByNumber = CutChoiceOneVoters.length;
+						};
+
+						if (ChoiceTwoVoters === 'No votes yet') {
+							ChoiceTwoVoters = 0;
+						} else {
+							ChoiceTwoVotersByNumber = CutChoiceTwoVoters.length;
+						};
+
+						i.reply(`**${ChoiceOneVotersByNumber}** votes for **${ChoiceOne}** and **${ChoiceTwoVotersByNumber}** votes for **${ChoiceTwo}**!`);
+
+						ChoiceOneVotersByNumber = 0;
+						ChoiceTwoVotersByNumber = 0;
+						ChoiceOneVoters = "";
+						ChoiceTwoVoters = "";
+						CutChoiceOneVoters = "";
+						CutChoiceTwoVoters = "";
+						MultiChoiceConfig = "";
+
+						const NewButtons = new MessageActionRow()
+						.addComponents(
+							new MessageButton()
+							.setCustomId('VoteOne')
+							.setEmoji('1️⃣')
+							.setStyle('SECONDARY')
+							.setDisabled(true),
+						)
+						.addComponents(
+							new MessageButton()
+							.setCustomId('VoteTwo')
+							.setEmoji('2️⃣')
+							.setStyle('SECONDARY')
+							.setDisabled(true),
+						)
+						.addComponents(
+							new MessageButton()
+							.setCustomId('EnableMultiChoice')
+							.setLabel('Enable Multi-Choice')
+							.setStyle('SUCCESS')
+							.setDisabled(true),
+						)
+						.addComponents(
+							new MessageButton()
+							.setCustomId('StopPoll')
+							.setLabel('Close the Poll')
+							.setStyle('DANGER')
+							.setDisabled(true),
+						);
+
+						sentMessage.edit({
+							components: [
+								NewButtons
+							],
+						}).catch((e) => {
+							console.log(e);
+							i.channel.send(`An error occured while trying to edit the poll!`);
+							return;
+						});
+
+					};
+
 				});
 			}),
 		);
